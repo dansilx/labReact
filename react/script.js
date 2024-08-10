b111f0a0c6a83e50664f1b6be839b9614f054145
dansilx <danilosilveira@msn.com>
2024-08-10 11:06:36 -0300

    add script

--- react/script.js
+++ react/script.js
@@ -1,3 +1,12 @@
 for (i=1;i <= 6; i++) {
     document.write(`<h${i}>Olá Sala!</h${i}>`);
 }
+
+let nome = prompt("Digite o seu nome:");
+if(confirm("Quer ver seu nome?")) {
+    alert("Seu nome é: " + nome);
+};
+
+x = 10;
+console.log(x);
+console.log(typeof(x));
