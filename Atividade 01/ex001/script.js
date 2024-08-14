449930a38768362f96bb0e7b631ff4955c0c1140
dansilx <danilosilveira@msn.com>
2024-08-13 23:22:24 -0300

    enviando o ex001 correto

--- Atividade 01/ex001/script.js
+++ Atividade 01/ex001/script.js
@@ -1,20 +1,18 @@
-document.getElementById("form").addEventListener('submit', function(event) {
-    event.preventDefault()
+const btn = document.getElementById("btn")
 
-    const btn = document.getElementById("btn")
+btn.addEventListener('click', function() {
     const input = document.getElementById("number").value
     const number = Number(input)
 
     if (!Number.isInteger(number) || number <= 0) {
         alert("Número incorreto!")
     }
-
-    if (number % 2 == 0)
-        { 
-            alert("Número par!")
-        }
-        else 
-        { 
-            alert("Número ímpar!")
-        }
+    else if (number % 2 == 0)
+    { 
+        alert(`${number} é um número par!`)
+    }
+    else 
+    { 
+        alert(`${number} é um número ímpar!`)        
+    }
 })
