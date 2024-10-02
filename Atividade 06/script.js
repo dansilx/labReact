2d4a723ecbd26df3b8f9e826735898f618fffd99
dansilx <danilosilveira@msn.com>
2024-10-02 17:09:26 -0300

    enviando react

--- Atividade 06/script.js
+++ Atividade 06/script.js
@@ -1,3 +1,18 @@
 const Letreiro = () => {
+    const [text, setText] = React.useState("");
+    const message = "Venha estudar na Fatec";
+    let i = 0;
     
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            setText((prevText) => prevText + message[i]);
+            index++;
+            if (i == message.length) clearInterval(interval);
+        }, 150);
+
+        return () => clearInterval(interval);
+    }, []);
+    return <h1>{text}</h1>;
 }
+
+ReactDOM.render(<Letreiro />, document.getElementById("root"));
