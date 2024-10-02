8d6e76fb6631a8a059842004bbe8277f2b486b88
dansilx <danilosilveira@msn.com>
2024-10-02 14:00:41 -0300

    enviar react

---
+++ Atividade 06/marquee-effect/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TypingEffect from './TypingEffect';
+
+const App = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f0f0f0' }}>
+      <TypingEffect />
+    </div>
+  );
+};
+
+export default App;
